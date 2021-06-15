143c363d2236d8b7e096cb5e2b855e580a7c453a
Quentin Van Bladel <44942@etu.he2b.be>
2021-06-15 23:44:56 +0200

    init backend

---
+++ backend/config/db.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+import colors from 'colors'
+
+
+
+
+const connectDB = async () => {
+    try{
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+        useCreateIndex: true
+    })
+
+    console.log(`MongoDB connected: ${conn.connection.host}`.cyan )
+    }catch(err){
+        console.error(`ERROR: ${err}`.red.underline.bold)
+        process.exit(1)
+    }
+
+}
+
+export default connectDB
